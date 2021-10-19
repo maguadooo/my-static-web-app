19ed1ea06865eefbc04894d051849261230d7996
maguadooo <maguado1992@gmail.com>
2021-10-19 15:53:34 +0200

    Create TestConfig.ps1

---
+++ TestConfig.ps1
@@ -0,0 +1,19 @@
+configuration TestConfig
+{
+    Node IsWebServer
+    {
+        WindowsFeature IIS {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+        }
+    }
+
+    Node NotWebServer
+    {
+        WindowsFeature IIS {
+            Ensure = 'Absent'
+            Name   = 'Web-Server'
+        }
+    }
+}
